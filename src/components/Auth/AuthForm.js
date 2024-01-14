f9cf7dd858f61a0bcf69584817c7caa67574b204
NivedHari <nivedhari44@gmail.com>
2024-01-14 16:19:05 +0530

    first commit

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,53 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isSending, setIsSending] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    setIsSending(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setIsSending(false);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required />
+        </div>
+        <div className={classes.actions}>
+          {!isSending && (
+            <button disabled={isSending}>
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )} {isSending && <p>Sending Request..</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
